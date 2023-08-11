5a39f138af450dfbc7abf958ba543285c064002f
bodwood <bodiewood@hotmail.com>
2023-08-11 13:38:54 -0700

    Initial Commit

---
+++ src/main/java/com/angularproject/employeemanager/model/Employee.java
@@ -0,0 +1,11 @@
+package com.angularproject.employeemanager.model;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phoneNumber;
+    private String imageUrl;
+
+}
